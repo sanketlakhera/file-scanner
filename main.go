2bf2ece140a8f52f64a734bc2c8bb4857456c1c7
Sanket <lakhera.sanket850@gmail.com>
2026-04-09 00:12:26 +0530

    feat: pointer

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func sayHello() {
-	fmt.Println("hello from goroutine")
-}
-
 func main() {
-	go sayHello() // we can mark any programm as goroutine by adding "go" keyword before the function call
-	fmt.Println("hello from main")
-	time.Sleep(time.Millisecond) // problem is that main function will exit before the goroutine function is called, that's why this little hack
+	x := 42
+	p := &x         // p holds the address of x
+	fmt.Println(p)  // 0xc0000b6010 (some memory address)
+	fmt.Println(*p) // 42
 }

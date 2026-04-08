51180cc65f0a5950fc63b0947dfee83e028bcb7d
Sanket <lakhera.sanket850@gmail.com>
2026-04-08 22:12:40 +0530

    feat: goroutine

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func sayHello() {
+	fmt.Println("hello from goroutine")
+}
 
 func main() {
-	fmt.Println("Hello, World!") // Hello, World!
+	go sayHello() // we can mark any programm as goroutine by adding "go" keyword before the function call
+	fmt.Println("hello from main")
+	time.Sleep(time.Millisecond) // problem is that main function will exit before the goroutine function is called, that's why this little hack
 }

0bc16409b4bff9b6091fb1feb8c346d728e49cb9
Sanket <lakhera.sanket850@gmail.com>
2026-04-15 01:16:22 +0530

    feat: table tests added

--- main_test.go
+++ main_test.go
@@ -3,10 +3,27 @@ package main
 import "testing"
 
 func TestFormatSize(t *testing.T) {
-	expected := "500 B"
-	actual := formatSize(500)
+	// define table of test cases
+	tests := []struct {
+		name     string
+		input    int64
+		expected string
+	}{
+		{"Bytes", 500, "500 B"},
+		{"Exact Kilobyte", 1024, "1.0 KB"},
+		{"Exact Megabyte", 1024 * 1024, "1.0 MB"},
+		{"Some Kilobyte", 1536, "1.5 KB"},
+		{"Some Megabyte", 1536 * 1024, "1.5 MB"},
+	}
 
-	if actual != expected {
-		t.Errorf("Expected %s, got %s", expected, actual)
+	// loop through test cases
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := formatSize(tt.input)
+
+			if actual != tt.expected {
+				t.Errorf("Expected %s, but got %s", tt.expected, actual)
+			}
+		})
 	}
 }
